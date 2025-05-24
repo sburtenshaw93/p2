5390e76b6077aca7d7263423a258ebbebccd5c58
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2025-05-24 13:10:07 -0600

    added if the fork() failed

--- p2.cpp
+++ p2.cpp
@@ -4,7 +4,12 @@
 #include <cstring> // to use all string and namespace function
 
 int main() {
+    pid_t pid = fork();
 
+    if (pid < 0) {
+        std::cout << "Fork failed" << std::endl;
+        return 1;
+    }
 
 
     return 0;
