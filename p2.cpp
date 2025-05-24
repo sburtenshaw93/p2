5f8cf00aa1894698330321f34cc5d018e61cb718
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2025-05-24 13:07:41 -0600

    Made main() and added extensions that will be used

---
+++ p2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sys/wait.h> // Allow interactions with child processes
+#include <unistd.h> // Portable Operationg System Interface API --> POSIX
+#include <cstring> // to use all string and namespace function
+
+int main() {
+
+
+
+    return 0;
+}
